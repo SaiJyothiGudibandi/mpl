02923893a257873f3d401498104dd7ff83e4ac94
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-03-30 14:46:56 -0700

    1

--- resources/com/griddynamics/devops/mpl/modules/Build/MavenBuild.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Build/MavenBuild.groovy
@@ -1,8 +1,5 @@
 /**
  * Simple Maven Build
  */
-
-withEnv(["PATH+MAVEN=${tool(CFG.'maven.tool_version' ?: 'Maven 3')}/bin"]) {
-  def settings = CFG.'maven.settings_path' ? "-s '${CFG.'maven.settings_path'}'" : ''
-  sh """mvn -B ${settings} -DargLine='-Xmx1024m -XX:MaxPermSize=1024m' clean install"""
-}
+// Run Maven on a Unix agent.
+sh "${params.build_cmd}"

53897d5f58c9875d82f52c77e224f7cb7e66ce6b
SaiJyothi Gudibandi <saijyothigudibandi9@gmail.com>
2020-03-30 15:34:45 -0700

    3

--- resources/com/griddynamics/devops/mpl/modules/Deploy/DockerDeploy.groovy
+++ resources/com/griddynamics/devops/mpl/modules/Deploy/DockerDeploy.groovy
@@ -2,9 +2,8 @@
  * Deploy the built Docker images
  */
 echo"test deploy"
-//sh "docker push saijyothi9/mlptest"
 sh "docker push ${params.docker_tag}"
 
-sh "docker stop \$(docker ps -a -q)"
-sh "docker rm \$(docker ps -a -q)"
+//sh "docker stop \$(docker ps -a -q)"
+//sh "docker rm \$(docker ps -a -q)"
 sh "docker run --name mynginx1 -p 80:80 -d ${params.docker_tag}"
